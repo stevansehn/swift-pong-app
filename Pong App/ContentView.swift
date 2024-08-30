fe8164378fe32fdc474238ca590f2e46e3653a57
Stevan Sehn <stevanlinse@gmail.com>
2024-08-30 15:29:13 -0300

    Initial Commit

---
+++ Pong App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pong App
+//
+//  Created by Stevan Sehn on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pong App/Pong_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pong_AppApp.swift
+//  Pong App
+//
+//  Created by Stevan Sehn on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Pong_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
